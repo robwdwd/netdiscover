fedff7bdd6db18c56cf143f60b62e0e5e244bd5c
Rob Woodward <rob@emailplus.org>
2024-09-11 15:01:07 +0000

    testing

--- src/netdiscover/cli.py
+++ src/netdiscover/cli.py
@@ -8,12 +8,37 @@
 import json
 import os
 import pprint
+import logging
+import re
 
 import click
+
 from scrapli.driver import GenericDriver
+
+logging.basicConfig(format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
+logger = logging.getLogger()
 
 pp = pprint.PrettyPrinter(indent=2, width=120)
 
+def compile_patterns() -> dict[str, re.Pattern]:
+    """Compile a dictionary of string patterns into regex patterns.
+
+    Args:
+        pattern_dict (dict[str, str]): A dictionary of keys and their associated string patterns.
+
+    Returns:
+        dict[str, Pattern]: A dictionary mapping each key to its compiled regex pattern.
+    """
+    return {
+      "IOS-XR": re.compile('Cisco IOS XR'),
+      "IOS-XE": re.compile('Cisco IOS-XE'),
+      "IOS": re.compile('Cisco IOS'),
+      "JunOS": re.compile('JunOS')
+    }
+
+
+def parse_output(output: str):
+    pass
 
 def connect(hostname: str, username: str, password: str):
 
@@ -22,6 +47,10 @@ def connect(hostname: str, username: str, password: str):
         "auth_username": username,
         "auth_password": password,
         "auth_strict_key": False,
+        "asyncssh": {
+            "kex_algs": "+diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha1",
+            "encryption_algs": "+3des-cbc",
+        }
     }
 
     # Context manager is a great way to use scrapli, it will auto open/close the connection for you:
@@ -29,7 +58,7 @@ def connect(hostname: str, username: str, password: str):
         conn.send_command("terminal length 0")
         result = conn.send_command("show version")
 
-    pp.pprint(result)
+    pp.pprint(result.result)
 
 
 def load_config(config_file):
@@ -48,8 +77,25 @@ def load_config(config_file):
     envvar="NETDISCOVER_CONFIG_FILE",
     type=click.File(mode="r"),
 )
+@click.option(
+    "--loglevel",
+    "-L",
+    type=str,
+    default="WARNING",
+    envvar="NETDISCOVER_LOG_LEVEL",
+    help="Set logging level.",
+)
+@click.option(
+    "-d",
+    "--device",
+    type=str,
+    metavar="HOSTNAME",
+    help="Single device to connect to",
+)
 def cli(**cli_args):
 
     cfg = load_config(cli_args["config"])
     pp.pprint(cfg)
-    connect('test', cfg['username'], cfg['password'])
+    logger.setLevel(cli_args["loglevel"].upper())
+
+    connect(cli_args['device'], cfg['username'], cfg['password'])
