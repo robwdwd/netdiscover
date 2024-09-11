d038d74edf5f9c542a5403b495daa3c8f5c818f0
Rob Woodward <rob@emailplus.org>
2024-09-11 14:17:17 +0100

    first code

--- src/netdiscover/cli.py
+++ src/netdiscover/cli.py
@@ -5,14 +5,37 @@
 # "BSD 2-Clause License". Please see the LICENSE file that should
 # have been included as part of this distribution.
 #
+import json
+import os
 import pprint
-from json import JSONDecodeError, load
+
 import click
+from scrapli.driver import GenericDriver
+
+pp = pprint.PrettyPrinter(indent=2, width=120)
+
+
+def connect(hostname: str, username: str, password: str):
+
+    MY_DEVICE = {
+        "host": hostname,
+        "auth_username": username,
+        "auth_password": password,
+        "auth_strict_key": False,
+    }
+
+    # Context manager is a great way to use scrapli, it will auto open/close the connection for you:
+    with GenericDriver(**MY_DEVICE) as conn:
+        conn.send_command("terminal length 0")
+        result = conn.send_command("show version")
+
+    pp.pprint(result)
+
 
 def load_config(config_file):
     try:
         return json.load(config_file)
-    except JSONDecodeError as err:
+    except json.JSONDecodeError as err:
         raise SystemExit(f"Unable to parse configuration file: {err}") from err
 
 
@@ -29,3 +52,4 @@ def cli(**cli_args):
 
     cfg = load_config(cli_args["config"])
     pp.pprint(cfg)
+    connect('test', cfg['username'], cfg['password'])
