d3ed81d4e97e8c4ae2c00f18e4f4b99f7cffa245
Rob Woodward <rob@emailplus.org>
2024-09-11 16:34:38 +0100

    first code

--- src/netdiscover/cli.py
+++ src/netdiscover/cli.py
@@ -5,19 +5,38 @@
 # "BSD 2-Clause License". Please see the LICENSE file that should
 # have been included as part of this distribution.
 #
+import asyncio
 import json
 import logging
 import os
 import pprint
 import re
+import tempfile
 
+import aiosqlite
 import click
-from scrapli.driver import GenericDriver
+
+from netdiscover.worker import DeviceWorker, DeviceWorkerException
 
 logging.basicConfig(format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
 logger = logging.getLogger()
 
 pp = pprint.PrettyPrinter(indent=2, width=120)
+
+async def setup_database(db_file: str) -> aiosqlite.Connection:
+    try:
+        db_con = await aiosqlite.connect(db_file)
+        db_cursor = await db_con.cursor()
+        await db_cursor.execute("DROP TABLE IF EXISTS devices")
+        await db_cursor.execute(
+            "CREATE TABLE devices(hostname, vendor, os, protocol)"
+        )
+        await db_con.commit()
+        await db_cursor.close()
+        return db_con
+    except aiosqlite.Error as err:
+        raise SystemExit(f"Failed to create new SQLite database: {err}") from err
+
 
 
 def compile_patterns() -> dict[str, re.Pattern]:
@@ -37,32 +56,55 @@ def compile_patterns() -> dict[str, re.Pattern]:
     }
 
 
-def parse_output(output: str):
-    pass
 
 
-def connect(hostname: str, username: str, password: str):
+async def do_devices(devices: list, prog_args: dict):
+    queue = asyncio.Queue()
+    db_lock = asyncio.Lock()
 
-    MY_DEVICE = {
-        "host": hostname,
-        "auth_username": username,
-        "auth_password": password,
-        "auth_strict_key": False,
-        "transport": 'ssh2',
-        "transport_options": {
-            "ssh2": {
-                "kex_algs": "+diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha1",
-                "encryption_algs": "+3des-cbc",
-            }
-        },
-    }
+    db_con = await setup_database(prog_args["db_file"])
 
-    # Context manager is a great way to use scrapli, it will auto open/close the connection for you:
-    with GenericDriver(**MY_DEVICE) as conn:
-        conn.send_command("terminal length 0")
-        result = conn.send_command("show version")
+    for device in devices:
+        await queue.put(device)
 
-    pp.pprint(result.result)
+    # Create three worker tasks to process the queue concurrently.
+    workers = [asyncio.create_task(DeviceWorker(db_con, db_lock, queue, prog_args).run(i)) for i in range(3)]
+
+    try:
+        await asyncio.gather(*workers, return_exceptions=False)
+    except DeviceWorkerException as err:
+        logger.error("Worker failed can not continue: %s", err)
+        for task in workers:
+            task.cancel()
+
+        await asyncio.gather(*workers, return_exceptions=True)
+        await db_con.close()
+        return
+
+    #await output_results(db_con, prog_args["out_format"], prog_args["quotechar"], prog_args["delimeter"])
+    await db_con.close()
+
+
+def setup_devices(cli_args: dict) -> list:
+    """
+    Setup devices based on command-line arguments.
+
+    Args:
+        cli_args (dict): Dictionary containing command-line arguments.
+
+    Returns:
+        list: list of devices.
+
+    Raises:
+        SystemExit: If required --seed or --device options are missing or there is an error decoding the JSON seed file.
+    """
+
+    if cli_args["device"]:
+        return [cli_args["device"]]
+    elif cli_args["seed"]:
+        return []
+    else:
+        raise SystemExit("Required --seed or --device options are missing.")
 
 
 def load_config(config_file):
@@ -102,4 +144,16 @@ def cli(**cli_args):
     pp.pprint(cfg)
     logger.setLevel(cli_args["loglevel"].upper())
 
-    connect(cli_args["device"], cfg["username"], cfg["password"])
+    devices = setup_devices(cli_args)
+
+    with tempfile.TemporaryDirectory(prefix="netdiscover_", suffix="_db", ignore_cleanup_errors=False) as tmp_db_dir:
+
+        prog_args = {
+            "username": cfg["username"],
+            "password": cfg["password"],
+            "db_file": f"{tmp_db_dir}/results.db",
+        }
+
+        asyncio.run(do_devices(devices, prog_args))
+
+
---
+++ src/netdiscover/worker.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2024, Rob Woodward. All rights reserved.
+#
+# This file is part of Net Discover tool Tool and is released under the
+# "BSD 2-Clause License". Please see the LICENSE file that should
+# have been included as part of this distribution.
+#
+
+import asyncio
+import logging
+import pprint
+
+import aiosqlite
+from scrapli.driver import AsyncGenericDriver
+
+pp = pprint.PrettyPrinter(indent=2, width=120)
+
+logger = logging.getLogger()
+
+
+class DeviceWorkerException(Exception):
+    """Device worker exception."""
+
+    pass
+
+
+class DeviceWorker:
+    """Device Worker."""
+
+    def __init__(
+        self,
+        db_con: aiosqlite.Connection,
+        db_lock: asyncio.Lock,
+        queue: asyncio.Queue,
+        prog_args: dict,
+    ) -> None:
+        """Init.
+
+        Args:
+            db_con (aiosqlite.Connection): SQlite DB Connection
+            db_lock (asyncio.Lock): Database Lock
+            queue (asyncio.Queue): Queue of devices
+            prog_args (dict): Program Args
+
+        Raises:
+            DeviceWorkerException: When worker does not start
+        """
+        self.db_con = db_con
+        self.queue = queue
+        self.db_lock = db_lock
+        self.prog_args = prog_args
+        self.db_cursor = None
+
+    def parse_output(self, output: str):
+        pass
+
+    async def connect(self, hostname: str, username: str, password: str):
+
+        MY_DEVICE = {
+            "host": hostname,
+            "auth_username": username,
+            "auth_password": password,
+            "auth_strict_key": False,
+            "transport": "asyncssh2",
+            "transport_options": {
+                "asyncssh2": {
+                    "kex_algs": "+diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha1",
+                    "encryption_algs": "+3des-cbc",
+                }
+            },
+        }
+
+        async with AsyncGenericDriver(**MY_DEVICE) as conn:
+            await conn.send_command("terminal length 0")
+            result = await conn.send_command("show version")
+
+        return result.result
+
+    async def run(self, i: int) -> None:
+        """Device worker coroutine, reads from the queue until empty."""
+
+        try:
+            self.db_cursor = await self.db_con.cursor()
+        except aiosqlite.Error as err:
+            raise DeviceWorkerException(f"Worker {i} failed to create db cursor: {err}") from err
+
+        try:
+            while not self.queue.empty():
+                device: str = self.queue.get_nowait()
+                result = []
+
+                try:
+                    result = await self.connect(device, self.prog_args['username'], self.prog_args['password'])
+                except Exception as err:
+                    logger.exception("[%s] Device failed: %s", device, err)
+                    self.queue.task_done()
+                    continue
+
+                if not result:
+                    logger.info("[%s] Device has no version output.", device)
+                    self.queue.task_done()
+                    continue
+
+                pp.pprint(result)
+
+                # async with self.db_lock:
+                #     try:
+                #         await self.db_cursor.executemany(
+                #             "INSERT INTO neighbours "
+                #             "(hostname,os,platform,address_family,ip_version,is_up,pfxrcd,protocol_instance,remote_asn,remote_ip,routing_instance,state) "
+                #             "VALUES(:hostname,:os,:platform,:address_family,:ip_version,:is_up,:pfxrcd,:protocol_instance,:remote_asn,:remote_ip,:routing_instance,:state);",
+                #             result,
+                #         )
+                #         await self.db_con.commit()
+                #     except aiosqlite.Error as err:
+                #         logger.exception("[%s] Failed to insert result in to database: %s", device.hostname, err)
+
+                self.queue.task_done()
+        except asyncio.CancelledError:
+            logger.info("Worker #%d was cancelled due to failure of other workers.", i)
+            raise
+        finally:
+            logger.info("Worker #%d finished, running cleanup.", i)
+            if self.db_cursor:
+                await self.db_cursor.close()
